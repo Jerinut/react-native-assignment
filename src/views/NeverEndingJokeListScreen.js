66e5ced3aa147ed81b584c17a205f70b4effad99
jeringeorge98 <jerringeorge439@gmail.com>
2024-06-24 22:58:03 +0300

    Task 3 done

--- src/views/NeverEndingJokeListScreen.js
+++ src/views/NeverEndingJokeListScreen.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import { View, Button, StyleSheet, Text } from 'react-native';
+import React,{ useEffect, useState,useFocusEffect }  from 'react';
+import { fetchRandomJoke } from '../store/apis/JokesSlice';
+import { useDispatch,useSelector } from 'react-redux';
+import { View, Button, StyleSheet, Text ,ScrollView} from 'react-native';
 
 export default function NeverEndingJokeListScreen({ navigation }) {
+  const dispatch = useDispatch();
+  const joke = useSelector((state) => state.joke.data);
+  const [jokes, setJokes] = useState([]);
+
+  const handlePress = () => {
+    dispatch(fetchRandomJoke());
+  };
+
+  useEffect(() => {
+    if (joke) {
+      setJokes((oldJokes) => [...oldJokes, joke]);
+    }
+  }, [joke]);
   return (
     <View style={styles.container}>
-       <Text>Never NeverEndingJokeListScreen</Text> 
+      <Button title="Press for a Chuck Norris Joke" onPress={handlePress} />
+      <ScrollView>
+        {jokes.map((joke, index) => (
+          <View key={index} style={styles.card}>
+            <Text>{joke.value}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -14,9 +36,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingTop: 10
   },
   btnContainer: {
-    flex:1,
-    margin:10,
-  }
+    margin: 10,
+  },
+  card: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    margin: 10,
+  },
 });
